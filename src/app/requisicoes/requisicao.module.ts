7ae1e858ee7375b962eac549f952471fdddc73cf
Pedro Nunes <pedronunesjc@gmail.com>
2022-09-09 16:44:31 -0300

    implementado roteamento interno

--- src/app/requisicoes/requisicao.module.ts
+++ src/app/requisicoes/requisicao.module.ts
@@ -5,11 +5,15 @@ import { RequisicaoRoutingModule } from './requisicao-routing.module';
 import { RequisicaoComponent } from './requisicao.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { RequisicoesFuncionarioComponent } from './requisicoes-funcionario/requisicoes-funcionario.component';
+import { RequisicoesDepartamentoComponent } from './requisicoes-departamento/requisicoes-departamento.component';
 
 
 @NgModule({
   declarations: [
-    RequisicaoComponent
+    RequisicaoComponent,
+    RequisicoesFuncionarioComponent,
+    RequisicoesDepartamentoComponent
   ],
   imports: [
     CommonModule,
