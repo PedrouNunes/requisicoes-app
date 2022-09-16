37d07a41c1c5bdab40ae33e570ba794ee6dde15b
Pedro Nunes <pedronunesjc@gmail.com>
2022-09-16 17:53:48 -0300

    ng build dado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDi3GyEDGGsR4F0pq-p4bW4vsjmDaQE-W4",
+    authDomain: "requisicoes-app-c8b1b.firebaseapp.com",
+    projectId: "requisicoes-app-c8b1b",
+    storageBucket: "requisicoes-app-c8b1b.appspot.com",
+    messagingSenderId: "119525095031",
+    appId: "1:119525095031:web:1d278508cd6d8a52b4b872"
+  }
 };
